717a181150237b18b8278b1fc019262155e0dc11
GiraffePencils <giraffes_eat_pencils@hotmail.co.uk>
2016-01-27 21:44:22 +0000

    Trying to send unreal changes up

--- TimeJump/Source/TimeJump/TimeJump.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "TimeJump.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeJump, "TimeJump" );
- 
--- TimeJump/Source/TimeJump/TimeJump.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __TIMEJUMP_H__
-#define __TIMEJUMP_H__
-
-#include "EngineMinimal.h"
-
-
-#endif
